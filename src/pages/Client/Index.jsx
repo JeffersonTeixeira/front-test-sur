326968de36814ef48f0186b4a781314056519b7f
jefferson <jeffersonteixeira@outlook.com>
2021-12-16 10:04:03 -0300

    Button add cli and msg no clients

--- src/pages/Client/Index.jsx
+++ src/pages/Client/Index.jsx
@@ -24,53 +24,46 @@ const Index = () => {
     }, [currentPage]);
 
     const fillClientList = () => {
-
-        if (page['content'] && page['content'].length) {
-            return page.content.map(cli => {
-                return (
-                    <tr key={cli.id}>
-                        <th scope="row">{cli.id}</th>
-                        <td>{cli.name}</td>
-                        <td><PhoneInput className="form-control-plaintext" value={cli.phone.number} readOnly={true} /></td>
-                        <td><InputMask mask='999.999.999-99' type="text" className="form-control-plaintext" value={cli.document} readOnly /></td>
-                        <td>{cli.email}</td>
-                        <td><Link to={`/client/${cli.id}`}>ver</Link></td>
-                    </tr>
-                )
-            });
-        }
+        return page.content.map(cli => {
+            return (
+                <tr key={cli.id}>
+                    <th scope="row">{cli.id}</th>
+                    <td>{cli.name}</td>
+                    <td><PhoneInput className="form-control-plaintext" value={cli.phone.number} readOnly={true} /></td>
+                    <td><InputMask mask='999.999.999-99' type="text" className="form-control-plaintext" value={cli.document} readOnly /></td>
+                    <td>{cli.email}</td>
+                    <td><Link to={`/client/${cli.id}`}>ver</Link></td>
+                </tr>
+            );
+        });
     }
 
     const renderPagination = () => {
-
-        if (page['content'] && page['content'].length) {
-            return (
-                <nav aria-label="Page navigation client">
-                    <ul className="pagination">
-                        <li className={'page-item ' + (page.first ? 'disabled' : '')}>
-                            <a className="page-link" onClick={() => setCurrentPage(currentPage - 1)}>Anterior</a>
-                        </li>
-                        {Array.apply(0, Array(page.totalPages)).map(function (x, i) {
-                            return (
-                                <li className={'page-item ' + (i == currentPage ? 'active' : '')} key={i} onClick={() => setCurrentPage(i)}>
-                                    <a className="page-link" >{i + 1}</a>
-                                </li>
-                            );
-                        })}
-                        <li className={'page-item ' + (page.last ? 'disabled' : '')}>
-                            <a className="page-link" onClick={() => setCurrentPage(currentPage + 1)}>Próxima</a>
-                        </li>
-                    </ul>
-                </nav>
-            )
-        }
+        return (
+            <nav aria-label="Page navigation client">
+                <ul className="pagination">
+                    <li className={'page-item ' + (page.first ? 'disabled' : '')}>
+                        <a className="page-link" onClick={() => setCurrentPage(currentPage - 1)}>Anterior</a>
+                    </li>
+                    {Array.apply(0, Array(page.totalPages)).map(function (x, i) {
+                        return (
+                            <li className={'page-item ' + (i == currentPage ? 'active' : '')} key={i} onClick={() => setCurrentPage(i)}>
+                                <a className="page-link" >{i + 1}</a>
+                            </li>
+                        );
+                    })}
+                    <li className={'page-item ' + (page.last ? 'disabled' : '')}>
+                        <a className="page-link" onClick={() => setCurrentPage(currentPage + 1)}>Próxima</a>
+                    </li>
+                </ul>
+            </nav>
+        );
     }
 
 
-    return (
-        <>
-            <Header title="Clientes" />
-            <div className="clients content container">
+    const renderTableClients = () => {
+        return (
+            <>
                 <table className="table table-striped table-hover">
                     <thead className="thead-dark">
                         <tr>
@@ -86,7 +79,28 @@ const Index = () => {
                         {fillClientList()}
                     </tbody>
                 </table>
-                {renderPagination()}
+                { renderPagination()}
+            </>
+        )
+    }
+
+
+    return (
+        <>
+            <Header title="Clientes" />
+            <div className="clients content container">
+                {
+                    page['content'] && page['content'].length ?
+                        renderTableClients()
+                        :
+                        <div class="alert alert-warning" role="alert">
+                            Nenhum cliente encontrado
+                        </div>
+                }
+                <div className="bg-light" >
+                    <Link className="btn btn-primary" to={`/client`}>Adicionar Cliente</Link>
+                </div>
+
             </div>
         </>
     );
